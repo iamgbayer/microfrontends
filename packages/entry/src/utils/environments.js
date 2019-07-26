8d466029e7b55625cfe1cd731b0f02086b9abc98
Guilherme Bayer <guiilherme.bayer@gmail.com>
2019-07-26 01:22:25 -0300

    Add production services URL

--- packages/entry/src/utils/environments.js
+++ packages/entry/src/utils/environments.js
@@ -4,8 +4,8 @@ const environments = {
     NAVIGATION_URL: 'http://localhost:8082/main.js'
   },
   prod: {
-    HOME_URL: 'http://localhost:8083/main.js',
-    NAVIGATION_URL: 'http://localhost:8082/main.js'
+    HOME_URL: 'https://home-6b31f1.netlify.com',
+    NAVIGATION_URL: 'https://navigation-d4e972.netlify.com'
   }
 }
 
