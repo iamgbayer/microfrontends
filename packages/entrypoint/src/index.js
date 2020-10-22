1710d894a04f422a252ac8821eb13b43a35626a1
Guilherme Bayer <iamgbayer@gmail.com>
2020-10-22 10:16:26 -0300

    Update index.js

--- packages/entrypoint/src/index.js
+++ packages/entrypoint/src/index.js
@@ -5,19 +5,19 @@ import { whenMatch } from './utils'
 
 registerApplication(
   '@microfrontends/home',
-  SystemJS.import('@microfrontends/home'),
+  () => SystemJS.import('@microfrontends/home'),
   whenMatch('/')
 )
 
 registerApplication(
   '@microfrontends/checkout',
-  SystemJS.import('@microfrontends/checkout'),
+  () => SystemJS.import('@microfrontends/checkout'),
   whenMatch('/checkout')
 )
 
 registerApplication(
   '@microfrontends/navigation',
-  SystemJS.import('@microfrontends/navigation'),
+  () => SystemJS.import('@microfrontends/navigation'),
   () => true
 )
 
